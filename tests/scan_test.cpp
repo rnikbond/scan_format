126a116d7b76a509f6d8f88ba2f4ccb119e5fa36
rnikbond <rnikbond@gmail.com>
2026-01-19 22:07:14 +0300

    Добавлены тесты

--- tests/scan_test.cpp
+++ tests/scan_test.cpp
@@ -3,7 +3,133 @@
 
 #include "scan.hpp"
 
-TEST(ScanTest, SimpleTest) {
-    auto result = stdx::scan<std::string>("number", "{}");
-    ASSERT_FALSE(result);
+/**
+ * @brief Проверка преобразования строки в int
+ * @details Ошибки не ожидается
+ */
+TEST(ScanFormatTest, CheckSupport_int) {
+    std::string input = "123";
+    std::string fmt = "{%d}";
+    auto result = stdx::scan<int>(input, fmt);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(result.value().value<0>(), 123);
+}
+
+/**
+ * @brief Проверка преобразования строки в double
+ * @details Ошибки не ожидается
+ */
+TEST(ScanFormatTest, CheckSupport_double) {
+    std::string input = "123.321";
+    std::string fmt = "{%f}";
+    auto result = stdx::scan<double>(input, fmt);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(result.value().value<0>(), 123.321);
+}
+
+/**
+ * @brief Проверка преобразования строки в string
+ * @details Ошибки не ожидается
+ */
+TEST(ScanFormatTest, CheckSupport_string) {
+    std::string input = "text text text";
+    std::string fmt = "{%s}";
+    auto result = stdx::scan<std::string>(input, fmt);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(result.value().value<0>(), input);
+}
+
+/**
+ * @brief Проверка выхода преобразованного значения за диапазон типа
+ * @details Ожидается ошибка
+ */
+TEST(ScanFormatTest, Check_int8_OutOfRange) {
+    std::string input = "200";
+    std::string fmt = "{%d}";
+    auto result = stdx::scan<int8_t>(input, fmt);
+    EXPECT_FALSE(result);
+}
+
+/**
+ * @brief  Проверка преобразования строки в int без указания формата
+ * @details Ошибки не ожидается
+ */
+TEST(ScanEmptyFormatTest, CheckSupport_int) {
+    std::string input = "123";
+    std::string fmt = "{}";
+    auto result = stdx::scan<int>(input, fmt);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(result.value().value<0>(), 123);
+}
+
+/**
+ * @brief  Проверка преобразования строки в int без указания формата
+ * @details Ошибки не ожидается
+ */
+TEST(ScanEmptyFormatTest, CheckSupport_double) {
+    std::string input = "123.321";
+    std::string fmt = "{}";
+    auto result = stdx::scan<double>(input, fmt);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(result.value().value<0>(), 123.321);
+}
+
+/**
+ * @brief Проверка преобразования строки в string без указания формата
+ * @details Ошибки не ожидается
+ */
+TEST(ScanEmptyFormatTest, CheckSupport_string) {
+    std::string input = "text text text";
+    std::string fmt = "{}";
+    auto result = stdx::scan<std::string>(input, fmt);
+    ASSERT_TRUE(result);
+    EXPECT_EQ(result.value().value<0>(), input);
+}
+
+/**
+ * @brief Проверка невозможности преобразования строки в указанный формат
+ * @details Ожидается ошибка
+ */
+TEST(ScanFailTest, CheckImpossibleParseInputToType) {
+    std::string input = "text text text";
+
+    auto result1 = stdx::scan<int>(input, "%d");
+    ASSERT_FALSE(result1);
+
+    auto result2 = stdx::scan<float>(input, "%f");
+    ASSERT_FALSE(result2);
+
+    auto result3 = stdx::scan<double>(input, "%d");
+    ASSERT_FALSE(result2);
+}
+
+/**
+ * @brief Проверка несоответствия типа и формата
+ * @details Ожидается ошибка
+ */
+TEST(ScanFailTest, CheckDiffTypeAndFormat) {
+    std::string input = "321";
+
+    auto result_int = stdx::scan<int>(input, "%s");
+    ASSERT_FALSE(result_int);
+
+    auto result_float = stdx::scan<float>(input, "%d");
+    ASSERT_FALSE(result_float);
+
+    auto result_str = stdx::scan<std::string>(input, "%u");
+    ASSERT_FALSE(result_str);
+}
+
+/**
+ * @brief Проверка несуществующего формата
+ * @details Ожидается ошибка
+ */
+TEST(ScanFailTest, CheckUnknownFormat) {
+    std::string input = "321";
+
+    std::vector<std::string> formats = {"%k", "%y", "%D", "%f", "$f", "#d", "%d", "%f", "%u"};
+    for (auto &fmt : formats) {
+        auto result = stdx::scan<int>(input, fmt);
+        ASSERT_FALSE(result);
+    }
 }
